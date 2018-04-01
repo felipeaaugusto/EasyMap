9b58153b1f56c3dd18f42922fd9032788c8e7e62
Felipe <felipeaugustobento@hotmail.com>
2018-04-01 16:20:53 -0300

    Adicionando post users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,11 +4,26 @@ var router = express.Router();
 var dbConnection = require('../db/db');
 var db = dbConnection.getDb();
 
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     db.collection("users").find({}).toArray(function(err, result) {
         if (err) throw err;
         res.send(result);
+    });
+});
+
+// POST method route
+router.post('/user', function (req, res) {
+    var myobj = { name: "Felipe", address: "Darioa Manoel" };
+    db.collection("users").insertOne(myobj, function(err, result) {
+        if (err) throw err;
+        res.send("1 document inserted");
     });
 });
 
