d59d9f4c5fb3c5c768da8ea6eb62e2bbad95cc68
Felipe <felipeaugustobento@hotmail.com>
2018-04-26 23:07:13 -0300

    Criando rota para listar geojson countries

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -12,9 +13,12 @@ var app = express();
 var dbConnection = require('./db/db');
 
 dbConnection.connectToServer(function(err){
+    app.use(cors());
+
     // start the rest of your app here
     var indexRouter = require('./routes/index');
     var usersRouter = require('./routes/users');
+    var countriesRouter = require('./routes/countries');
     
     // view engine setup
     app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +32,7 @@ dbConnection.connectToServer(function(err){
     
     app.use('/', indexRouter);
     app.use('/', usersRouter);
+    app.use('/', countriesRouter);
     
     // catch 404 and forward to error handler
     app.use(function(req, res, next) {
@@ -49,3 +54,5 @@ dbConnection.connectToServer(function(err){
 });
 
 module.exports = app;
+
+// enable CORS => https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
